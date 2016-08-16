f1fab34c40cc5fc975ecfdfc49678b73694ce544
Maggie Correll <maggie.correll@gmail.com>
2016-08-16 11:06:20 -0400

    add register

--- cms_app/forms.py
+++ cms_app/forms.py
@@ -3,3 +3,11 @@ from django import forms
 
 class BlogForm(forms.Form):
     description = forms.Textarea()
+
+
+class Register(forms.Form):
+    username = forms.CharField(max_length=50)
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
