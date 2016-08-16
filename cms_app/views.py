d158b62c5fb984846b9794fe8e2c4722c1b2c0d6
Tara Davis <taradavis@Taras-MacBook-Pro.local>
2016-08-16 10:59:30 -0400

    Started views

--- cms_app/views.py
+++ cms_app/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import BlogPost
+from .forms import TaskForm
 
+
+# Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    if request.method == 'POST':
+        description = request.POST['post']
+        status = request.POST['status']
+        post = BlogPost(description=description, status=status)
+        post.save()
+    #
+    # dict_ = {
+    #     "ready": 1,
+    #     "doing": 2,
+    #     "backlog": 3,
+    #     "done": 4
+    # }
+    posts = sorted(BlogPost.objects.all())  # key=lambda x: dict_[x.status])
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'index.html', context)
+
+# tasks = Task.objects.all().order_by("-updated")
